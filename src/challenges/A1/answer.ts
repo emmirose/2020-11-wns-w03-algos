9e23df6e81a6471ae476b9b5704bde4bdb0029b8
Emma Rosso <emmarosso.pro@gmail.com>
2023-12-18 10:27:16 +0100

    A1

--- src/challenges/A1/answer.ts
+++ src/challenges/A1/answer.ts
@@ -1,17 +1,23 @@
 /**
  * In this challenge, you must sort students by their age (younger first). If some of them have
  * the same age, then you should sort them alphabetically (A to Z)
- * 
+ *
  * @param students Unsorted list of students
  * @returns Sorted list of students
  */
 
 // ↓ uncomment bellow lines and add your response!
-/*
+
 export default function ({ students }: { students: Student[] }): Student[] {
-    return [];
+    students.sort((a, b) => {
+        if (a.age === b.age) {
+            return a.name.localeCompare(b.name);
+        }
+        return a.age - b.age;
+    });
+    return students;
 }
-*/
+
 
 // used interfaces, do not touch
 export interface Student {
